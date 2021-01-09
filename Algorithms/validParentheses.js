8750562e06f0eac5d33a2ecc84f4ef8ce3d0b532
Beekasha <brian.j.kleiner@gmail.com>
2021-01-09 13:34:32 -0500

    validParentheses optimized

--- Algorithms/validParentheses.js
+++ Algorithms/validParentheses.js
@@ -1,19 +1,43 @@
+// let validParentheses = s => {
+//     let stack = [];
+
+//     for (let char of s){
+//         if (char === '(' || char === '[' || char === '{'){
+//             stack.push(char)
+//         }else{
+//             if (stack.length === 0) return false; //if closing bracket is first
+//             const last = stack.pop();
+//             if (last === '(' && char !== ')') {
+//                 return false;
+//             }else if(last === '{' && char !== '}') {
+//                 return false;
+//             }else if(last === '[' && char !== ']') {
+//                 return false;
+//             }
+//         }
+//     }
+//     if (stack.length){
+//         return false;
+//     }else{
+//         return true;
+//     }
+// }
+
 let validParentheses = s => {
     let stack = [];
+    const pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
 
     for (let char of s){
-        if (char === '(' || char === '[' || char === '{'){
+        if (char in pairs){
             stack.push(char)
         }else{
             if (stack.length === 0) return false; //if closing bracket is first
             const last = stack.pop();
-            if (last === '(' && char !== ')') {
-                return false;
-            }else if(last === '{' && char !== '}') {
-                return false;
-            }else if(last === '[' && char !== ']') {
-                return false;
-            }
+            if (pairs[last] !== char) return false;
         }
     }
     if (stack.length){
